cc5f1296302164f540764908323fa5b6905b7cae
Dana Goosen <klymentovska@gmail.com>
2025-05-18 15:18:06 +0200

    Add Tree and Player implementation

--- camera.py
+++ camera.py
@@ -2,10 +2,39 @@ import pygame, sys
 from random import randint
 
 class Tree(pygame.sprite.Sprite):
-    pass
+	def __init__(self,pos,group):
+		super().__init__(group)
+		self.image = pygame.image.load('graphics/tree.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
 
 class Player(pygame.sprite.Sprite):
-    pass
+	def __init__(self,pos,group):
+		super().__init__(group)
+		self.image = pygame.image.load('graphics/player.png').convert_alpha()
+		self.rect = self.image.get_rect(center = pos)
+		self.direction = pygame.math.Vector2()
+		self.speed = 5
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_UP]:
+			self.direction.y = -1
+		elif keys[pygame.K_DOWN]:
+			self.direction.y = 1
+		else:
+			self.direction.y = 0
+
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+		else:
+			self.direction.x = 0
+
+	def update(self):
+		self.input()
+		self.rect.center += self.direction * self.speed
 
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
